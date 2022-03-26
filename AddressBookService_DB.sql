a345b5f33451f86d35327e1cf19efca0737b76fc
Vibhauc_27 <ucvibha@gmail.com>
2022-03-26 08:13:17 +0530

    [Vibha]Added:To create table for address book service

--- AddressBookService_DB.sql
+++ AddressBookService_DB.sql
@@ -4,3 +4,20 @@
 create database AddressBookService_DB;
 
 use AddressBookService_DB;
+
+--UC2 
+--Ability to create a Address Book Table with first and last names, address, city, state, zip, phone number and email as its attributes
+
+create table AddressBook_Table
+(
+id int primary key identity(1,1),
+FirstName varchar(100),
+LastName varchar(100),
+Address varchar(100),
+City varchar(100),
+State varchar (100),
+Zip bigint,
+PhoneNumber bigint,
+Email varchar(100)
+);
+select * from AddressBook_Table;
